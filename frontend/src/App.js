0efc0f173815b5fc2a4ef342f01d7910ea120da0
Krushant <krushantw@gmail.com>
2024-07-13 08:37:12 +0530

    UI updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Cart } from './Pages/Cart';
 
 function App() {
   return (
     <div >
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="mens" />}/>
+        <Route path='/womens' element={<ShopCategory category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids" />}/>
+        <Route path='prduct' element= {<Product/>} >
+          <Route path=':prdoctId' element={<Product/>} />
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Hero } from '../Components/Hero/Hero'
+import { Popular } from '../Components/Popular/Popular'
+
+export const Shop = () => {
+  return (
+    <div>
+        <Hero/>
+        <Popular/>
+    </div>
+  )
+}
